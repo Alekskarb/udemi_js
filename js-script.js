85a8d2712ae572cb1f1425225cf372c328b1c141
Alekskarb <korbutowski@gmail.com>
2020-04-01 20:27:32 +0300

    'first'

--- js-script.js
+++ js-script.js
@@ -2,19 +2,23 @@ let money = prompt('Ваш бюджет на месяц?', '');
 let time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
-    budjet: money,
+    budget: money,
     timeData: time,
     expenses: {},
     optionalExpenses: {},
     income: [],
     savings: false,
 };
-let out = prompt('Введите обязательную статью расходов в этом месяце?', '');
-let sum = prompt('Во сколько обойдется?', '');
-let outer = prompt('Введите обязательную статью расходов в этом месяце?', '');
-let summ = prompt('Во сколько обойдется?', '');
+for (let i=0; i<1; i++){
+    let out = prompt('Введите обязательную статью расходов в этом месяце?', '');
+    let sum = prompt('Во сколько обойдется?', '');
+    if (typeof (out) ==='string' && typeof (out) !=null && typeof (sum) !=null
+        && out !=='' && sum !=='' && out.length <5 && sum.length <5) {appData.expenses[out] = sum;break}
 
-appData.expenses[out] = sum;
-appData.expenses.outer = summ;
+    else {continue}
+}
+
+appData.moneyDay= appData.budget/30;
+
 console.log(appData);
-alert(appData.budjet/30);
+alert(appData.moneyDay);
