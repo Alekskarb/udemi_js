e3eff68da9f1a07e2d4bae3516cd92e5ebcb2f5a
Alekskarb <korbutowski@gmail.com>
2020-04-14 20:50:14 +0300

    'five'

--- DZ_5/js/script.js
+++ DZ_5/js/script.js
@@ -6,9 +6,14 @@ liFive.textContent ='Пятый пункт';
 ulli.appendChild(liFive);
 let removed = document.querySelector('.adv');
 let good = document.querySelector('.title');
-// let pict = document.getElementsByTagName('body');
-// pict.replaceChild('body2', 'body')
+let qu = document.getElementsById('prompt');
+let answer = function ()
+{prompt('do you like Apple?', '');};
+answer();
+qu.appendChild(answer);
+
 removed.remove();
 // document.body.style.background = 'url(../img/apple_true.jpg)'
 document.body.style.background = 'url(file:///D:/%231_INcubator/%231_HTML_CSS_JS/UDEMY/udemy_js/DZ_5/img/apple_true.jpg)'
 good.textContent = 'Мы продаем только подлинную технику Apple'
+
