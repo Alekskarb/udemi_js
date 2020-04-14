d2673c23d5eac85b41e4ecf50b7221ba32e36ce4
Alekskarb <korbutowski@gmail.com>
2020-04-14 20:34:54 +0300

    'five'

--- DZ_5/js/script.js
+++ DZ_5/js/script.js
@@ -5,9 +5,10 @@ liFive.textContent ='Пятый пункт';
 
 ulli.appendChild(liFive);
 let removed = document.querySelector('.adv');
+let good = document.querySelector('.title');
 // let pict = document.getElementsByTagName('body');
 // pict.replaceChild('body2', 'body')
 removed.remove();
-document.body.style.background = '/#1_INcubator/#1_HTML_CSS_JS/UDEMY/udemy_js/DZ_№5/img/apple_true.jpg'
+// document.body.style.background = 'url(../img/apple_true.jpg)'
 document.body.style.background = 'url(file:///D:/%231_INcubator/%231_HTML_CSS_JS/UDEMY/udemy_js/DZ_5/img/apple_true.jpg)'
-
+good.textContent = 'Мы продаем только подлинную технику Apple'
