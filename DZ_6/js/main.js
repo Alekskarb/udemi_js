cba9f4cebca9cc7e25940d3e4ce49d1a6a842bb2
Alekskarb <korbutowski@gmail.com>
2020-04-15 11:14:18 +0300

    'six'

---
+++ DZ_6/js/main.js
@@ -0,0 +1,37 @@
+let startBtn = document.querySelector('#start');
+
+let blocks = document.querySelectorAll('.result-table div');
+let inputs = document.querySelectorAll('.expenses-item');
+let inputsOpt = document.querySelectorAll('.optionalexpenses-item');
+let buttonArray = document.getElementsByTagName('button');
+let apply = buttonArray[1];
+let calc = buttonArray[2];
+
+
+
+let news =[];
+    blocks.forEach((item, ind)=> {
+        if (ind % 2 === 1) {
+            news.push(item)
+        }});
+console.log(inputsOpt);
+
+// let liFive = document.createElement('li')
+// liFive.classList.add("menu-item");
+// liFive.textContent ='Пятый пункт';
+//
+// ulli.appendChild(liFive);
+// let removed = document.querySelector('.adv');
+// removed.remove();
+// let good = document.querySelector('.title');
+// let qu = document.getElementById('prompt');
+// let txt ='';
+// let answer = function () {txt = prompt('do you like Apple?', '');};
+// answer();
+// qu.textContent = txt;
+//
+//
+// // document.body.style.background = 'url(../img/apple_true.jpg)'
+// document.body.style.background = 'url(file:///D:/%231_INcubator/%231_HTML_CSS_JS/UDEMY/udemy_js/DZ_5/img/apple_true.jpg)'
+// good.textContent = 'Мы продаем только подлинную технику Apple'
+
